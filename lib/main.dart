2d4a5943c6f1efdc1fa962a2e3f881d51cf213fa
fulldown <fulldown@gmail.com>
2024-04-07 12:04:46 +0900

    03. Tabpage에 화면 연결

--- lib/main.dart
+++ lib/main.dart
@@ -16,9 +16,9 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        useMaterial3: false,
       ),
       home: const TabPage(),
     );
   }
 }
-
---
+++ lib/tab/home/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Instagram Clone'),
+      ),
+      body: const Column(
+        children: [Text('Instagram에 오신 것을 환영합니다.')],
+      ),
+    );
+  }
+}
